c629618ff10f2237d8f751ac7d7ce0346f0373f2
mxnueh <enmanuelnunezmatias@gmail.com>
2025-03-19 14:55:53 -0400

    Borrando el mensaje de conexion exitosa

--- config.php
+++ config.php
@@ -8,8 +8,6 @@ $conexion = new mysqli($servidor, $usuario, $clave, $base_de_datos);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
-} else {
-    echo "La conexion fue exitosa";
 }
 
 ?>
